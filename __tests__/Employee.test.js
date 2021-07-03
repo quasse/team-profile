e2e7076d1416c12f1649d0bb8ef74f3ddc38fa5a
Sam Morgen <samorgen14@gmail.com>
2021-07-03 12:30:52 -0700

    Added employee class

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,33 @@
+const Employee = require("../lib/Employee.js");
+
+test("Creates an employee object", () => {
+  const employee = new Employee("Sam", 1, "samorgen14@gmail.com");
+
+  expect(employee.name).toEqual(expect.any(String));
+  expect(employee.id).toEqual(expect.any(Number));
+  expect(employee.email).toEqual(expect.any(String));
+});
+
+test("gets an employee's name", () => {
+  const employee = new Employee("Sam", 1, "samorgen14@gmail.com");
+
+  expect(employee.getName()).toEqual(employee.name);
+});
+
+test("gets an employee's id", () => {
+  const employee = new Employee("Sam", 1, "samorgen14@gmail.com");
+
+  expect(employee.getId()).toEqual(employee.id);
+});
+
+test("gets an employee's email", () => {
+  const employee = new Employee("Sam", 1, "samorgen14@gmail.com");
+
+  expect(employee.getEmail()).toEqual(employee.email);
+});
+
+test("gets an employee's role", () => {
+  const employee = new Employee("Sam", 1, "samorgen14@gmail.com");
+
+  expect(employee.getRole()).toEqual("Employee");
+});
